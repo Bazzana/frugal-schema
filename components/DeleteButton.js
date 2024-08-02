39e1323543b4e83619458d6904c1483be05f5e6e
Ryan Bazzana <ryan@lambagency.com.au>
2024-08-02 15:06:43 +1000

    Fix delete button style

--- components/DeleteButton.js
+++ components/DeleteButton.js
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 export function DeleteButton({onClick}) {
     
     return(
-      <Button onClick={onClick} variant="destructive" size="icon">
+      <Button onClick={onClick} variant="destructive" className="w-8">
         <Trash2 className="h-4 w-4" />
       </Button>
     )
