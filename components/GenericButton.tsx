8b348d5b60bc564b164e744338cc366ff50b5177
Ryan Bazzana <ryan@lambagency.com.au>
2024-08-03 21:13:24 +1000

    Add loading prop

--- components/GenericButton.tsx
+++ components/GenericButton.tsx
@@ -1,8 +1,8 @@
 import { Button } from "@/components/ui/button"
+import { Loader2 } from "lucide-react"
 
-export function GenericButton({onClick, buttonText, disabled}) {
-    
-  return <Button disabled={disabled} onClick={onClick}>{buttonText}</Button>
+export function GenericButton({onClick, buttonText, disabled, isLoading}) {
+  return <Button disabled={disabled} onClick={onClick}>{isLoading == true ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : ''} {buttonText}</Button>
 }
 
 export default GenericButton
